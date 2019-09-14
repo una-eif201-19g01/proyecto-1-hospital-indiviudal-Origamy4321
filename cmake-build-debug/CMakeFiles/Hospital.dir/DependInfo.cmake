95c8d49a9830ff335f44ae0ad31c09e0ef6968f3
Greyvin Tercero Ruiz <53482798+Origamy4321@users.noreply.github.com>
2019-09-14 01:09:01 -0500

    archivos adicionales

---
+++ cmake-build-debug/CMakeFiles/Hospital.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Cama.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Cama.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Doctor.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Doctor.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Especialidad.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Especialidad.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Hospital.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Hospital.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Interfaz.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Interfaz.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Pabellon.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Pabellon.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/Paciente.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/Paciente.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/arregloCama.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/arregloCama.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/arregloDoctor.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/arregloDoctor.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/arregloEspecialidad.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/arregloEspecialidad.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/arregloPabellon.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/arregloPabellon.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/arregloPaciente.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/arregloPaciente.cpp.obj"
+  "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/main.cpp" "C:/Users/Lady Asura III/Desktop/FINAL_1/FINAL/cmake-build-debug/CMakeFiles/Hospital.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
